260a820bf7d7abc53df18c5d1c366f257d6d60f2
Remedeae <sanna.m.blomdahl@gmail.com>
2024-12-02 01:42:03 +0100

    added list priority functions

--- remedy-react-app/src/Pages/to-do-list.jsx
+++ remedy-react-app/src/Pages/to-do-list.jsx
@@ -1,23 +1,33 @@
 import { useState } from "react";
+import './to-do-list.css';
 
 function List() {
 
-    const [tasks, setTasks] = useState(["Clean litter-box", "Cook food"]);
+    const [tasks, setTasks] = useState(["Clean litter-box", "Cook food", "Pet the cat", "Buy a new rug", "Cause trouble"]);
 
     function handleAddTask() {
         const newTask = document.getElementById("taskInput").value;
         document.getElementById("taskInput").value = "";
 
-        setTasks ((t) => [...t, newTask]);
+        setTasks((t) => [...t, newTask]);
+
     }
     function handleRemoveTask(index) {
         setTasks(tasks.filter((_, i) => i !== index))
     }
-    function handleTaskPrioUp() {
-
+    //I do not understand how to execute things in react, there are so many different things. So this works if you
+    //update by using the button, but that's not how it should work.
+    function handleTaskPrioUp(index) {
+        let prevIndex = index - 1;
+        let nextIndex = index + 1;
+        tasks.splice(prevIndex, 0, tasks[index]);
+        tasks.splice(nextIndex, 1);
     }
-    function handleTaskPrioDown() {
-
+    function handleTaskPrioDown(index) {
+        let nextIndex = index + 1;
+        let secoundNextIndex = index + 2;
+        tasks.splice(index, 0, tasks[nextIndex]);
+        tasks.splice(secoundNextIndex, 1);
     }
 
     return (
@@ -27,10 +37,13 @@ function List() {
                 {tasks.map((task, index) => (
                     <li key={index}>
                         <div>
-                        <i className="fa-solid fa-sort-up" onClick={() => handleTaskPrioUp}></i>
-                        <i className="fa-solid fa-sort-down" onClick={() => handleTaskPrioDown}></i>
+                            <div className="arrow-icons">
+                                <i className="fa-solid fa-sort-up" onClick={() => handleTaskPrioUp(index)}></i>
+                                <i className="fa-solid fa-sort-down" onClick={() => handleTaskPrioDown(index)}></i>
+                            </div>
+                            <p style={{ margin: "10px 0" }}>{task}</p>
+                            {/* very ugly solution, but I can't find the user style sheet  */}
                         </div>
-                        <p>{task}</p>
                         <i className="fa-solid fa-trash" onClick={() => handleRemoveTask(index)}></i>
                     </li>
                 ))}
